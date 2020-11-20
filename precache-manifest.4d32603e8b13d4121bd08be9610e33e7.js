8663d6436c2fa92ac70b42e5cf519dbc204308e6
bevan <bevan.chan@harcourts.net>
2020-11-20 15:35:06 +1300

    Updates

---
+++ precache-manifest.4d32603e8b13d4121bd08be9610e33e7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c9e68dd6268b120dcad7ea4cc20225c",
+    "url": "/sample-01/index.html"
+  },
+  {
+    "revision": "b22c177f595776b07e3c",
+    "url": "/sample-01/static/css/2.11b452ed.chunk.css"
+  },
+  {
+    "revision": "8e756640235b8feda7d0",
+    "url": "/sample-01/static/css/main.1aa3f63c.chunk.css"
+  },
+  {
+    "revision": "b22c177f595776b07e3c",
+    "url": "/sample-01/static/js/2.49e8fb83.chunk.js"
+  },
+  {
+    "revision": "713acd2dac6c7d6e42a0e19bc8c5c98d",
+    "url": "/sample-01/static/js/2.49e8fb83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e756640235b8feda7d0",
+    "url": "/sample-01/static/js/main.9da59787.chunk.js"
+  },
+  {
+    "revision": "d8b2493fffa4a8b88423",
+    "url": "/sample-01/static/js/runtime-main.b07cdce9.js"
+  },
+  {
+    "revision": "c5590569aab553af03722baa7d379978",
+    "url": "/sample-01/static/media/loading.c5590569.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sample-01/static/media/logo.5d5d9eef.svg"
+  }
+]);
